3e7436dafa174d5af81241c1dce9785378a52fb3
sebahernandez <sebaprogramer@gmail.com>
2024-12-29 23:41:41 -0300

    cambio de vista y enlaces a vehiculos dinamicos

--- src/components/catalog/VehiculeCard.tsx
+++ src/components/catalog/VehiculeCard.tsx
@@ -1,14 +1,13 @@
 import type { Datum } from "../../types/vehicule";
+import { formatNameForURL } from "../../helpers/stringHelpers";
+import { formatPrice } from "../../helpers/formatHelpers";
 
 interface VehicleCardProps {
   readonly vehicle: Datum;
 }
 
 export function VehicleCard({ vehicle }: VehicleCardProps) {
-  const formattedPrice = new Intl.NumberFormat("es-CL", {
-    style: "currency",
-    currency: "CLP",
-  }).format(Number(vehicle.price.replace(/[^\d]/g, "")));
+  const formattedPrice = formatPrice(vehicle.price);
 
   return (
     <div className="bg-gray-900 rounded-lg shadow-lg overflow-hidden h-full border border-gray-800">
@@ -19,7 +18,13 @@ export function VehicleCard({ vehicle }: VehicleCardProps) {
       />
       <div className="p-4">
         {/* <span className="text-sm text-gray-400">{vehicle.year}</span> */}
-        <h3 className="font-bold text-xl mb-2 text-white">{vehicle.name}</h3>
+        <h3 className="font-bold text-xl mb-2 text-white">
+          <a
+            href={`/vehiculos/${formatNameForURL(vehicle.name)}/${vehicle.id}`}
+          >
+            {vehicle.name}
+          </a>
+        </h3>
         <p className="text-2xl font-bold text-yellow-400 mb-4">
           {formattedPrice}
         </p>
